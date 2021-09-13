e2d65e2ee1dcccb1d9e1d7e0ddf6c8be9063b28a
ajbeattie <austin.beattie@canonical.com>
2021-09-13 13:25:23 -0500

    update build script

--- scripts/build-jenkins.sh
+++ scripts/build-jenkins.sh
@@ -11,12 +11,12 @@ DOCKER=${DOCKER:-docker}
 
 VERSION=`git rev-parse --verify HEAD`
 
-$DOCKER Dockerfile.ubuntu build \
+$DOCKER build \
 	--build-arg http_proxy \
 	--build-arg https_proxy \
 	--build-arg no_proxy \
 	--build-arg NO_PROXY \
 	-t ${DOCKER_REGISTRY}/pushgateway-ubuntu:$VERSION \
-  -t ${DOCKER_REGISTRY}/pushgateway-ubuntu:latest .
+  -t ${DOCKER_REGISTRY}/pushgateway-ubuntu:latest ./Dockerfile.ubuntu
 
 $DOCKER push --all-tags ${DOCKER_REGISTRY}/pushgateway-ubuntu
